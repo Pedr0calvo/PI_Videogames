4ab116c3dd38d04295a3200d71cd546641aa6cb0
Pedro Calvo <pedro7calvo@gmail.com>
2022-08-30 13:44:39 -0300

    final1

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,19 +1,21 @@
 import style from "./Footer.module.css";
+import linkeind from "./Icons/linkedin-square-logo-24.png";
+import github from './Icons/github-logo-24.png'
 
 export const Footer = () => {
   return (
     <div className={style.footer}>
       <a href="https://github.com/Pedr0calvo">
         <img
-          src="https://pngimg.com/uploads/github/github_PNG58.png"
+          src={github}
           alt="notfound"
           className={style.img}
         ></img>
       </a>
-      <div>🕹️ | Created by Pedro</div>
+      <div>🕹️ | Created by Pedro Calvo</div>
       <a href="https://www.linkedin.com/in/pedro-calvo/">
         <img
-          src="http://pngimg.com/uploads/linkedIn/small/linkedIn_PNG16.png"
+          src={linkeind}
           alt="notfound"
           className={style.img}
         ></img>
